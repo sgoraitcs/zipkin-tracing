85f8ccba692d4f10c05440c4f5ea78378860434f
Shyam Gorai <spg.144@gmail.com>
2019-12-08 16:18:51 +0530

    Zipkin tracing created

---
+++ src/main/java/com/salesorder/microservice/zipkintracing/ZipkinTracingApplication.java
@@ -0,0 +1,16 @@
+package com.salesorder.microservice.zipkintracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinTracingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesorder/microservice/zipkintracing/ZipkinTracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesorder.microservice.zipkintracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinTracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
